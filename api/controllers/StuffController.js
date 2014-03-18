b12d1639c5e459c18f70e140627fb72142dd41e1
Ryan Kelley <ryan@ninecollective.com>
2014-03-18 10:58:23 -0500

    Added a schema-less model and controller

---
+++ api/controllers/StuffController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by JF05563 on 3/18/14.
+ */
+    // This controller is for the Schemaless model of Stuff, you can put any attributes you want
+var StuffController = {
+
+}
+module.exports = StuffController;
---
+++ api/models/Stuff.js
@@ -0,0 +1,8 @@
+/**
+ * Created by JF05563 on 3/18/14.
+ */
+// This model is Schema-Less - You can put any attributes you want
+var Stuff = {
+
+}
+module.exports = Stuff;
